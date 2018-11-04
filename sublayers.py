a23b1abec27b561478ba551c9acfb4d96f483a88
Jiyang Zhang <zhanjiya@octal02.iro.umontreal.ca>
2018-11-04 14:19:08 -0500

    update the multihead-attention part

--- sublayers.py
+++ sublayers.py
@@ -14,37 +14,28 @@ class MultiHeadAttention(nn.Module):
   Multi-head attention as per https://arxiv.org/pdf/1706.03762.pdf
   Refer Figure 2
   """
-  def __init__(self, input_depth, total_key_depth, total_value_depth, output_depth, 
-         num_heads, bias_mask=None, dropout=0.0):
+  def __init__(self, word_dim, query_length, doc_length, num_heads, output_dim
+    bias_mask=None, dropout=0.0):
     """
     Parameters:
-      input_depth: Size of last dimension of input
-      total_key_depth: Size of last dimension of keys. Must be divisible by num_head
-      total_value_depth: Size of last dimension of values. Must be divisible by num_head
-      output_depth: Size last dimension of the final output
+      word_dim: The dimension of word embedding
+      query_length: The number of words in the query
+      doc_length: The number of words in the document
       num_heads: Number of attention heads
       bias_mask: Masking tensor to prevent connections to future elements
       dropout: Dropout probability (Should be non-zero only during training)
     """
     super(MultiHeadAttention, self).__init__()
     # Checks borrowed from 
-    # https://github.com/tensorflow/tensor2tensor/blob/master/tensor2tensor/layers/common_attention.py
-    if total_key_depth % num_heads != 0:
-      raise ValueError("Key depth (%d) must be divisible by the number of "
-               "attention heads (%d)." % (total_key_depth, num_heads))
-    if total_value_depth % num_heads != 0:
-      raise ValueError("Value depth (%d) must be divisible by the number of "
-               "attention heads (%d)." % (total_value_depth, num_heads))
       
-    self.num_heads = num_heads
-    self.query_scale = (total_key_depth//num_heads)**-0.5
+    self.num_heads = num_heads  # Number of attention heads
+    self.query_scale = (word_dim//num_heads)**-0.5
     self.bias_mask = bias_mask
     
     # Key and query depth will be same
-    self.query_linear = nn.Linear(input_depth, total_key_depth, bias=False)
-    self.key_linear = nn.Linear(input_depth, total_key_depth, bias=False)
-    self.value_linear = nn.Linear(input_depth, total_value_depth, bias=False)
-    self.output_linear = nn.Linear(total_value_depth, output_depth, bias=False)
+    self.linear_trans = nn.Linear(word_dim, word_dim, bias=False)
+    
+    self.output_linear = nn.Linear(word_dim, output_dim, bias=False)
     
     self.dropout = nn.Dropout(dropout)
   
@@ -77,12 +68,12 @@ class MultiHeadAttention(nn.Module):
   def forward(self, queries, keys, values):
     
     # Do a linear for each component
-    queries = self.query_linear(queries)
-    keys = self.key_linear(keys)
-    values = self.value_linear(values)
+    queries = self.linear_trans(queries) 
+    keys = self.linear_trans(keys)
+    values = self.linear_trans(values)
     
     # Split into multiple heads
-    queries = self._split_heads(queries)
+    queries = self._split_heads(queries) # SHAPE=[batch_size, num_heads, seq_length, dim/num_heads]
     keys = self._split_heads(keys)
     values = self._split_heads(values)
     
@@ -90,53 +81,28 @@ class MultiHeadAttention(nn.Module):
     queries *= self.query_scale
     
     # Combine queries and keys
-    logits = torch.matmul(queries, keys.permute(0, 1, 3, 2))
+    logits = torch.matmul(queries, keys.permute(0, 1, 3, 2))  #SHAPE=[bs, num_heads, query_length, doc_length]
     
     # Add bias to mask future values
     if self.bias_mask is not None:
       logits += Variable(self.bias_mask[:, :, :logits.shape[-2], :logits.shape[-1]].type_as(logits.data))
     
     # Convert to probabilites
-    weights = nn.functional.softmax(logits, dim=-1)
+    weights = nn.functional.softmax(logits, dim=2)  # PROBLEM ! dim 
     
     # Dropout
     weights = self.dropout(weights)
     
     # Combine with values to get context
-    contexts = torch.matmul(weights, values)
+    contexts = torch.matmul(weights, values)  #SHAPE=[bs, num_heads, query_length, dim/num_heads]
     
     # Merge heads
-    contexts = self._merge_heads(contexts)
+    contexts = self._merge_heads(contexts)   # SHAPE= [batch_size, query_length, dim]
     #contexts = torch.tanh(contexts)
     
     # Linear to get output
     outputs = self.output_linear(contexts)
     
-    return outputs
-
-class Conv(nn.Module):
-  """
-  Convenience class that does padding and convolution for inputs in the format
-  [batch_size, sequence length, hidden size]
-  """
-  def __init__(self, input_size, output_size, kernel_size, pad_type):
-    """
-    Parameters:
-      input_size: Input feature size
-      output_size: Output feature size
-      kernel_size: Kernel width
-      pad_type: left -> pad on the left side (to mask future data), 
-            both -> pad on both sides
-    """
-    super(Conv, self).__init__()
-    padding = (kernel_size - 1, 0) if pad_type == 'left' else (kernel_size//2, (kernel_size - 1)//2)
-    self.pad = nn.ConstantPad1d(padding, 0)
-    self.conv = nn.Conv1d(input_size, output_size, kernel_size=kernel_size, padding=0)
-
-  def forward(self, inputs):
-    inputs = self.pad(inputs.permute(0, 2, 1))
-    outputs = self.conv(inputs).permute(0, 2, 1)
-
     return outputs
 
 
