c1a9cd440d7cd293155971ce8b34cf4744bf9baf
JiyangZhang <33189384+JiyangZhang@users.noreply.github.com>
2018-11-06 15:15:20 -0500

    update the model_train.py

--- model_train.py
+++ model_train.py
@@ -239,96 +239,133 @@ def int_handler(sess, model_path, saver, signal, frame):
     print('successfully saved model to {}'.format(model_path))
     sys.exit(0)
 
+
+
+
 def train(config_path, resume_training=False):
-    '''training process'''
-    # parse config
+    """
+    Load the parameters
+    """
     config = configparser.ConfigParser()
     config.read(config_path)
-    '''hyper params'''
+
+    batch_sampling_method = config['params']['batch_sampling_method']
     model_name_str = config['hyperparams']['model_name_str']
+    
+    '''common parameters'''
     batch_size = ast.literal_eval(config['hyperparams']['batch_size'])  # batch_size
     vocab_size = ast.literal_eval(config['hyperparams']['vocab_size'])  # vocab_size
     emb_size = ast.literal_eval(config['hyperparams']['emb_size'])  # embedding dimension
     hidden_size = ast.literal_eval(config['hyperparams']['hidden_size'])
     dropout = ast.literal_eval(config['hyperparams']['dropout'])
-    q_filt1 = ast.literal_eval(config['hyperparams']['q_filt1'])
-    q_filt2 = ast.literal_eval(config['hyperparams']['q_filt2'])
-    q_filt3 = ast.literal_eval(config['hyperparams']['q_filt3'])
-    q_stride1 = ast.literal_eval(config['hyperparams']['q_stride1'])
-    q_stride2 = ast.literal_eval(config['hyperparams']['q_stride2'])
-    q_stride3 = ast.literal_eval(config['hyperparams']['q_stride3'])
-    d_filt1 = ast.literal_eval(config['hyperparams']['d_filt1'])
-    d_filt2 = ast.literal_eval(config['hyperparams']['d_filt2'])
-    d_filt3 = ast.literal_eval(config['hyperparams']['d_filt3'])
-    d_stride1 = ast.literal_eval(config['hyperparams']['d_stride1'])
-    d_stride2 = ast.literal_eval(config['hyperparams']['d_stride2'])
-    d_stride3 = ast.literal_eval(config['hyperparams']['d_stride3'])
-    d_pool0 = ast.literal_eval(config['hyperparams']['d_pool0'])
-    d_pool1 = ast.literal_eval(config['hyperparams']['d_pool1'])
-    d_pool2 = ast.literal_eval(config['hyperparams']['d_pool2'])
-    d_pool3 = ast.literal_eval(config['hyperparams']['d_pool3'])
-    d_plstride0 = ast.literal_eval(config['hyperparams']['d_plstride0'])
-    d_plstride1 = ast.literal_eval(config['hyperparams']['d_plstride1'])
-    d_plstride2 = ast.literal_eval(config['hyperparams']['d_plstride2'])
-    d_plstride3 = ast.literal_eval(config['hyperparams']['d_plstride3'])
-    Mtopk = ast.literal_eval(config['hyperparams']['Mtopk'])
     preemb = ast.literal_eval(config['hyperparams']['preemb'])
     sim_type = config['hyperparams']['sim_type']
     hinge_margin = ast.literal_eval(config['hyperparams']['hinge_margin'])
-    train_datablock_size = ast.literal_eval(config['hyperparams']['train_datablock_size'])
-    # for sampleQ sampleD prepare_data()
-    q_sample_size = ast.literal_eval(config['hyperparams']['q_sample_size'])
-    docpair_sample_size = ast.literal_eval(config['hyperparams']['docpair_sample_size'])
+    emb_tune = ast.literal_eval(config['commons']['emb_tune'])
     n_epoch = ast.literal_eval(config['hyperparams']['n_epoch']) # num of epochs
-    alpha = ast.literal_eval(config['hyperparams']['alpha'])  # weight decay
-    learning_rate = ast.literal_eval(config['hyperparams']['learning_rate'])
     # q and doc cuts
     q_len = ast.literal_eval(config['hyperparams']['q_len'])
     d_len = ast.literal_eval(config['hyperparams']['d_len'])
     # base_path
     data_base_path = config['hyperparams']['data_base_path']  #'/scratch/nyfbb'
     model_base_path = config['hyperparams']['model_base_path']  #'/home/nyfbb/exp'
-    # pool_mode
+    # dataset and fold
+    dataset = config['commons']['dataset']
+    fold = ast.literal_eval(config['commons']['fold'])
+    
+    '''representation hyperparams'''
+    filt_size_rep = ast.literal_eval(config['rep']['filt_size'])
+    kernel_size_rep = ast.literal_eval(config['rep']['kernel_size'])
+    output_dim_rep = ast.literal_eval(config['rep']['output_dim'])
+
+    # for sampleQ sampleD prepare_data()
+    q_sample_size = ast.literal_eval(config['hyperparams']['q_sample_size'])
+    docpair_sample_size = ast.literal_eval(config['hyperparams']['docpair_sample_size'])
+    
+    alpha = ast.literal_eval(config['hyperparams']['alpha'])  # weight decay
+    learning_rate = ast.literal_eval(config['hyperparams']['learning_rate'])
+
+    # new hyper parameters
+    num_heads = ast.literal_eval(config['hyperparams']['num_heads'])
+    emb_path = ast.literal_eval(config['hyperparams']['emb_path'])
+    # parameters that maybe useless
     pool_mode = config['hyperparams']['pool_mode']
+    Mtopk = ast.literal_eval(config['hyperparams']['Mtopk'])
+
     # use cuda flag
     use_cuda = True
-    '''TRAINING DIR'''
-    TRAIN_DIR011 = '{}/aol/train_nnrank011/'.format(data_base_path)
-    TRAIN_DIR021 = '{}/aol/train_nnrank021/'.format(data_base_path)
-    TRAIN_DIR031 = '{}/aol/train_nnrank031/'.format(data_base_path)
-    TRAIN_DIR061 = '{}/aol/train_nnrank061/'.format(data_base_path)
-    TRAIN_DIR071 = '{}/aol/train_nnrank071/'.format(data_base_path)
-    TRAIN_DIR081 = '{}/aol/train_nnrank081/'.format(data_base_path)
-    TRAIN_DIR091 = '{}/aol/train_nnrank091/'.format(data_base_path)
-    TRAIN_DIR101 = '{}/aol/train_nnrank101/'.format(data_base_path)
-    train_files011 = glob.glob("{}/data*.pkl".format(TRAIN_DIR011))
-    train_files021 = glob.glob("{}/data*.pkl".format(TRAIN_DIR021))
-    train_files031 = glob.glob("{}/data*.pkl".format(TRAIN_DIR031))
-    train_files061 = glob.glob("{}/data*.pkl".format(TRAIN_DIR061))
-    train_files071 = glob.glob("{}/data*.pkl".format(TRAIN_DIR071))
-    train_files081 = glob.glob("{}/data*.pkl".format(TRAIN_DIR081))
-    train_files091 = glob.glob("{}/data*.pkl".format(TRAIN_DIR091))
-    train_files101 = glob.glob("{}/data*.pkl".format(TRAIN_DIR101))
-    train_files = train_files011 + train_files021 + train_files031 + train_files061 \
-    + train_files071 + train_files081 + train_files091 + train_files101
-
-
     
-    '''build model'''
-    model = Attention(word_dim, query_length, doc_length, num_heads, 
-                bias_mask=None, dropout=0.0, input_dim, hidden_size, batch_size, preemb_path)
-    print(sum(p.numel() for p in model.parameters()))
+
+    """
+    the tranining directory
+    """
+    # load data
+    TRAIN_DIR01 = "{}/MQ2007/S1/".format(data_base_path)
+    TRAIN_DIR02 = "{}/MQ2007/S2/".format(data_base_path)
+    TRAIN_DIR03 = "{}/MQ2007/S3/".format(data_base_path)
+    TRAIN_DIR04 = "{}/MQ2007/S4/".format(data_base_path)
+    TRAIN_DIR05 = "{}/MQ2007/S5/".format(data_base_path)
+
+    TEST_DIR01 = '{}/MQ2007/S1/'.format(data_base_path)
+    TEST_DIR02 = '{}/MQ2007/S2/'.format(data_base_path)
+    TEST_DIR03 = '{}/MQ2007/S3/'.format(data_base_path)
+    TEST_DIR04 = '{}/MQ2007/S4/'.format(data_base_path)
+    TEST_DIR05 = '{}/MQ2007/S5/'.format(data_base_path)
+
+    train_files01 = glob.glob("{}/data0.pkl".format(TRAIN_DIR01))
+    train_files02 = glob.glob("{}/data0.pkl".format(TRAIN_DIR02))
+    train_files03 = glob.glob("{}/data0.pkl".format(TRAIN_DIR03))
+    train_files04 = glob.glob("{}/data0.pkl".format(TRAIN_DIR04))
+    train_files05 = glob.glob("{}/data0.pkl".format(TRAIN_DIR05))
+
+    test_files01 = glob.glob("{}/testdata0.pkl".format(TEST_DIR01))
+    test_files02 = glob.glob("{}/testdata0.pkl".format(TEST_DIR02))
+    test_files03 = glob.glob("{}/testdata0.pkl".format(TEST_DIR03))
+    test_files04 = glob.glob("{}/testdata0.pkl".format(TEST_DIR04))
+    test_files05 = glob.glob("{}/testdata0.pkl".format(TEST_DIR05))
+
+    if fold == 1:
+        train_files = train_files01 + train_files02 + train_files03
+        test_files = test_files04[0]  # a path list ['/...'] only take the str
+        rel_path = '{}/{}/tmp/test/S4.qrels'.format(model_base_path, model_name_str)
+    elif fold == 2:
+        train_files = train_files02 + train_files03 + train_files04
+        test_files = test_files05[0]
+        rel_path = '{}/{}/tmp/test/S5.qrels'.format(model_base_path, model_name_str)
+    elif fold == 3:
+        train_files = train_files03 + train_files04 + train_files05
+        test_files = test_files01[0]
+        rel_path = '{}/{}/tmp/test/S1.qrels'.format(model_base_path, model_name_str)
+    elif fold == 4:
+        train_files = train_files04 + train_files05 + train_files01
+        test_files = test_files02[0]
+        rel_path = '{}/{}/tmp/test/S2.qrels'.format(model_base_path, model_name_str)
+    elif fold == 5:
+        train_files = train_files05 + train_files01 + train_files02
+        test_files = test_files03[0]
+        rel_path = '{}/{}/tmp/test/S3.qrels'.format(model_base_path, model_name_str)
+    else:
+        raise ValueError("wrong fold num {}".format(fold))
+    
+    """!!!!!!!!!!"""    
+    X = prepare_train_data(0, 3, q_len, d_len, train_files)
+    valid_data = load_dataset(test_files)
 
 
+    """
+    Build the model
+    """
+    model = Attention(word_dim=emb_size, query_length=q_len, doc_length=d_len, num_heads=num_heads, 
+        kernel_size=kernel_size_rep, filter_size=filt_size, vocab_size=vocab_size, bias_mask=None,
+        dropout=0.0, qrep_dim=output_dim_rep, hidden_size=hidden_size,  batch_size=batch_size, preemb=preemb,
+        preemb_path=emb_path)
 
     if use_cuda:
         model.cuda()
     # optimizer
-    #optimizer = optim.SGD(model.parameters(), lr=learning_rate)
-    #learnable_parameters = filter(lambda p: p.requires_grad, model.parameters())
     optimizer = optim.Adam(model.parameters(), lr=learning_rate, weight_decay=alpha)
     # loss func
-    #loss = nn.MarginRankingLoss(margin=hinge_margin, size_average=True)
+    loss = nn.MarginRankingLoss(margin=hinge_margin, size_average=True)
     # experiment
     print("Experiment")
     if resume_training == False:
@@ -341,10 +378,9 @@ def train(config_path, resume_training=False):
     model_file = '{}/{}/saves/model_file'.format(model_base_path, model_name_str)
     
 
-    """TRAINING"""
-    # listen interrupt signal, pass model path to int_handler to save model
-    # signal.signal(signal.SIGINT, partial(int_handler, sess, model_file, saver))
-    # run init variables
+    """
+    TRAINING
+    """
 
     # init best validation MAP value
     best_MAP = 0.0
@@ -364,67 +400,63 @@ def train(config_path, resume_training=False):
     else:
         Y = Variable(torch.ones(batch_size), requires_grad=False)
     for epoch in range(1, n_epoch + 1):
-        train_files = list_shuffle(train_files)
-        for f in train_files:
-            '''load_data'''
-            data = load_dataset(f)
-            print("loaded {}".format(f))
-            '''prepare_data'''
-            [Q, D_pos, D_neg, L] = prepare_data_sampleQ_BM25distro(data, q_sample_size, docpair_sample_size, q_len)
-            ''' shuffle data'''
-            train_data = list_shuffle(Q, D_pos, D_neg, L)
-            '''training func'''
-            batch_count_tr = 0
-            num_batch = len(train_data[0]) // batch_size
-            for batch_count in range(num_batch):
-                Q = train_data[0][batch_size * batch_count: batch_size * (batch_count + 1)]
-                D_pos = train_data[1][batch_size * batch_count: batch_size * (batch_count + 1)]
-                D_neg = train_data[2][batch_size * batch_count: batch_size * (batch_count + 1)]
-                L = train_data[3][batch_size * batch_count: batch_size * (batch_count + 1)]
-                if use_cuda:
-                    Q = Variable(torch.LongTensor(pad_batch_list(Q, max_len=q_len, padding_id=0)), requires_grad=False).cuda()
-                    D_pos = Variable(torch.LongTensor(pad_batch_list(D_pos, max_len=d_len, padding_id=0)), requires_grad=False).cuda()
-                    D_neg = Variable(torch.LongTensor(pad_batch_list(D_neg, max_len=d_len, padding_id=0)), requires_grad=False).cuda()
-                    L = Variable(torch.FloatTensor(L), requires_grad=False).cuda()
-                else:
-                    Q = Variable(torch.LongTensor(pad_batch_list(Q, max_len=q_len, padding_id=0)), requires_grad=False)
-                    D_pos = Variable(torch.LongTensor(pad_batch_list(D_pos, max_len=d_len, padding_id=0)), requires_grad=False)
-                    D_neg = Variable(torch.LongTensor(pad_batch_list(D_neg, max_len=d_len, padding_id=0)), requires_grad=False)
-                    L = Variable(torch.FloatTensor(L), requires_grad=False)
-                # run on this batch
-                optimizer.zero_grad()
-                t1 = time.time()
-                q_mask, d_pos_mask, d_neg_mask = model.generate_mask(Q, D_pos, D_neg)
-                """
-                need to modify
-                """
-                S_pos, S_neg = model(Q, D_pos, D_neg, q_mask, d_pos_mask, d_neg_mask)
-                Loss = hinge_loss(S_pos, S_neg, 1.0)
-                Loss.backward()
-                optimizer.step()
-                t2 = time.time()
-                batch_count_tr += 1
-                print("epoch {} batch {} training cost: {} using {}s" \
-                .format(epoch, batch_count_tr, Loss.data[0], t2-t1))
-                f_log.write("epoch {} batch {} training cost: {}, using {}s".format(epoch, batch_count_tr, Loss.data[0], t2 - t1) + '\n')
-            file_count += 1
-            if file_count % 4 == 0:
-                # do rapid validation
-                ndcg_list, mapvalue = validation(model, model_name_str,
-                                                 data_base_path, model_base_path)  # pass the training compuational graph placeholder to valid function to evaluate with the same set of parameters
-                print("epoch :{}, pkl count: {}, NDCG".format(epoch, file_count), ndcg_list)
-                print("MAP: {}".format(mapvalue))
-                # check if this valid period is the best and update best_MAP, save model to disk
-                if mapvalue > best_MAP:
-                    best_MAP = mapvalue
-                    with open('{}/{}/saves/best_MAP.pkl'.format(model_base_path, model_name_str), 'wb') as f_MAP:
-                        pickle.dump(best_MAP, f_MAP)
-                    # save model params after several epoch
-                    model_file = '{}/{}/saves/model_file'.format(model_base_path, model_name_str)
-                    torch.save(model.state_dict(), model_file)
-                    print("successfully saved model to the path {}".format(model_file))
-                valid_log.write("{} {} {} {}".format(ndcg_list[1][0], ndcg_list[1][1], ndcg_list[1][2], ndcg_list[1][3]))
-                valid_log.write(" MAP: {}".format(mapvalue))
-                valid_log.write('\n')
+        '''load_data'''
+        data = load_dataset(train_files)
+        print("loaded {}".format(train_files))
+        '''prepare_data'''
+        [Q, D_pos, D_neg, L] = prepare_data_sampleQ_BM25distro(data, q_sample_size, docpair_sample_size, q_len)
+        ''' shuffle data'''
+        train_data = list_shuffle(Q, D_pos, D_neg, L)
+        '''training func'''
+        batch_count_tr = 0
+        num_batch = len(train_data[0]) // batch_size
+        for batch_count in range(num_batch):
+            Q = train_data[0][batch_size * batch_count: batch_size * (batch_count + 1)]
+            D_pos = train_data[1][batch_size * batch_count: batch_size * (batch_count + 1)]
+            D_neg = train_data[2][batch_size * batch_count: batch_size * (batch_count + 1)]
+            L = train_data[3][batch_size * batch_count: batch_size * (batch_count + 1)]
+            if use_cuda:
+                Q = Variable(torch.LongTensor(pad_batch_list(Q, max_len=q_len, padding_id=0)), requires_grad=False).cuda()
+                D_pos = Variable(torch.LongTensor(pad_batch_list(D_pos, max_len=d_len, padding_id=0)), requires_grad=False).cuda()
+                D_neg = Variable(torch.LongTensor(pad_batch_list(D_neg, max_len=d_len, padding_id=0)), requires_grad=False).cuda()
+                L = Variable(torch.FloatTensor(L), requires_grad=False).cuda()
+            else:
+                Q = Variable(torch.LongTensor(pad_batch_list(Q, max_len=q_len, padding_id=0)), requires_grad=False)
+                D_pos = Variable(torch.LongTensor(pad_batch_list(D_pos, max_len=d_len, padding_id=0)), requires_grad=False)
+                D_neg = Variable(torch.LongTensor(pad_batch_list(D_neg, max_len=d_len, padding_id=0)), requires_grad=False)
+                L = Variable(torch.FloatTensor(L), requires_grad=False)
+            
+            # run on this batch
+            optimizer.zero_grad()
+            t1 = time.time()
+            q_mask, d_pos_mask, d_neg_mask = model.generate_mask(Q, D_pos, D_neg)
+            S_pos, S_neg = model(Q, D_pos, D_neg, q_mask, d_pos_mask, d_neg_mask)
+            Loss = hinge_loss(S_pos, S_neg, 1.0)
+            Loss.backward()
+            optimizer.step()
+            t2 = time.time()
+            batch_count_tr += 1
+            print("epoch {} batch {} training cost: {} using {}s" \
+            .format(epoch, batch_count_tr, Loss.data[0], t2-t1))
+            f_log.write("epoch {} batch {} training cost: {}, using {}s".format(epoch, batch_count_tr, Loss.data[0], t2 - t1) + '\n')
+        file_count += 1
+        if file_count % 4 == 0:
+            # do rapid validation
+            ndcg_list, mapvalue = validation(model, model_name_str,
+                                             data_base_path, model_base_path)  # pass the training compuational graph placeholder to valid function to evaluate with the same set of parameters
+            print("epoch :{}, pkl count: {}, NDCG".format(epoch, file_count), ndcg_list)
+            print("MAP: {}".format(mapvalue))
+            # check if this valid period is the best and update best_MAP, save model to disk
+            if mapvalue > best_MAP:
+                best_MAP = mapvalue
+                with open('{}/{}/saves/best_MAP.pkl'.format(model_base_path, model_name_str), 'wb') as f_MAP:
+                    pickle.dump(best_MAP, f_MAP)
+                # save model params after several epoch
+                model_file = '{}/{}/saves/model_file'.format(model_base_path, model_name_str)
+                torch.save(model.state_dict(), model_file)
+                print("successfully saved model to the path {}".format(model_file))
+            valid_log.write("{} {} {} {}".format(ndcg_list[1][0], ndcg_list[1][1], ndcg_list[1][2], ndcg_list[1][3]))
+            valid_log.write(" MAP: {}".format(mapvalue))
+            valid_log.write('\n')
     f_log.close()
     valid_log.close()
